c0768c9a701d244f236664aba5ff992661cec2c1
Doyun <doyun@talktomeinkorean.com>
2022-09-21 16:04:02 +0900

    feature: android location permmision check

--- functions/dateFunction.ts
+++ functions/dateFunction.ts
@@ -1,22 +1,40 @@
-import Geolocation from '@react-native-community/geolocation';
+import {PermissionsAndroid, Platform} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
-const pushAttendance = (recordArray: any[], setRecordFunc: any) => {
+const pushAttendance = async (recordArray: any[], setRecordFunc: any) => {
+  // date generator
   const date = new Date();
   const realtime = `${date.getDate()}/${
     date.getMonth() + 1
   }/${date.getFullYear()}/${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
-  console.log(recordArray);
-  Geolocation.getCurrentPosition(position => {
-    const location = {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    };
-    return setRecordFunc([
-      ...recordArray,
-      {time: realtime, location: location},
-    ]);
-  });
+  // android location permission request
+  if (Platform.OS === 'android') {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+    console.log(granted);
+  }
+
+  // get location information
+  Geolocation.getCurrentPosition(
+    position => {
+      console.log(position);
+      const location = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      };
+      return setRecordFunc([
+        ...recordArray,
+        {time: realtime, location: location},
+      ]);
+    },
+    error => {
+      // See error code charts below.
+      console.log(error.code, error.message);
+    },
+    {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+  );
 };
 
 export default pushAttendance;
---
+++ functions/requestPermissions.ts
@@ -0,0 +1,20 @@
+import {Platform, PermissionsAndroid} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+
+async function requestPermissions() {
+  if (Platform.OS === 'ios') {
+    const auth = await Geolocation.requestAuthorization('whenInUse');
+    if (auth === 'granted') {
+      // do something if granted...
+    }
+  }
+
+  if (Platform.OS === 'android') {
+    await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      // do something if granted...
+    }
+  }
+}
